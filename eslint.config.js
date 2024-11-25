89f9960e55a21cac9b6bdc1af6228c934d0b1afe
Juniyasyos <juniyasyos@gmail.com>
2024-11-25 20:02:39 +0700

    fix: linting

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      "no-console": "warn",
+    },
+  },
+];
